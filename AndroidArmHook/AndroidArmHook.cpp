2f7fc5376dbcfc133489a33099433ea6b74859c2
晓梦大师/XMDS <2841824304@qq.com>
2023-01-29 12:19:15 +0800

    Add files via upload

---
+++ AndroidArmHook/AndroidArmHook.cpp
@@ -0,0 +1,297 @@
+#include "AndroidArmHook.h"
+
+#include <stdio.h> //snprintf fopen
+#include <string.h> //strcpy strstr
+#include <stdlib.h> //strtoul
+#include <errno.h>
+#include <sys/mman.h> //mprotect
+
+#include "HLog.h"
+#include "xDL/xdl.h"
+
+uintptr_t GetLibBase(const char* libName, pid_t pid)
+{
+    uintptr_t address = 0;
+    char buffer[2048] = { 0 }, fname[2048] = { 0 };
+    if (pid < 0)
+        strcpy(fname, "/proc/self/maps");
+    else
+        snprintf(fname, sizeof(fname), "/proc/%d/maps", pid);
+    FILE* fp = fopen(fname, "rt");
+    if (fp != NULL)
+    {
+        while (fgets(buffer, sizeof(buffer) - 1, fp))
+        {
+            if (strstr(buffer, libName))
+            {
+                address = (uintptr_t)strtoul(buffer, NULL, 16);
+                break;
+            }
+        }
+        fclose(fp);
+    }
+    return address;
+}
+
+size_t GetLibLength(const char* libName, pid_t pid)
+{
+    uintptr_t address = 0, end_address = 0;
+    char buffer[2048] = { 0 }, fname[2048] = { 0 };
+    if (pid < 0)
+        strcpy(fname, "/proc/self/maps");
+    else
+        snprintf(fname, sizeof(fname), "/proc/%d/maps", pid);
+    FILE* fp = fopen(fname, "rt");
+    if (fp != NULL)
+    {
+        while (fgets(buffer, sizeof(buffer) - 1, fp))
+        {
+            if (strstr(buffer, libName))
+            {
+                const char* secondPart = strchr(buffer, '-');
+                if (!address)
+                    end_address = address = (uintptr_t)strtoul(buffer, NULL, 16);
+                if (secondPart != NULL)
+                    end_address = (uintptr_t)strtoul(secondPart + 1, NULL, 16);
+            }
+        }
+        fclose(fp);
+    }
+    return end_address - address;
+}
+
+lib_h GetLibHandle(const char* libName)
+{
+    void* xdl_handle = xdl_open(libName, XDL_TRY_FORCE_LOAD);
+    if (NULL == xdl_handle) {
+        if (NULL != dlopen(libName, RTLD_LAZY))
+            xdl_handle = xdl_open(libName, XDL_DEFAULT);
+    }
+    return xdl_handle;
+}
+
+void CloseLib(lib_h handle)
+{
+    auto dl_handle = xdl_close(handle);
+    if (NULL != dl_handle)
+        dlclose(dl_handle);
+    return;
+}
+
+uintptr_t GetLibBaseFromHandle(lib_h handle)
+{
+    xdl_info_t info;
+    if (xdl_info(handle, XDL_DI_DLINFO, &info) == -1)
+        return NULL;
+    return (uintptr_t)info.dli_fbase;
+}
+
+const char* GetLibFilePath(uintptr_t libAddr)
+{
+    xdl_info_t info;
+    void* cache = NULL;
+    if (xdl_addr((void*)libAddr, &info, &cache) == 0)
+        return NULL;
+    xdl_addr_clean(&cache);
+    return info.dli_fname;
+}
+
+const char* GetLibFilePathFromHandle(lib_h handle)
+{
+    xdl_info_t info;
+    if (xdl_info(handle, XDL_DI_DLINFO, &info) == -1)
+        return NULL;
+    return info.dli_fname;
+}
+
+size_t GetLibFileSize(lib_h handle)
+{
+    size_t size = 0;
+    FILE* file = fopen(GetLibFilePathFromHandle(handle), "r");
+    if (file != NULL) {
+        fseek(file, 0, SEEK_END);
+        size = ftell(file);
+        fclose(file);
+    }
+    return size;
+}
+
+uintptr_t GetSymbolAddress(lib_h handle, const char* name)
+{
+    void* addr = xdl_sym(handle, name, NULL);
+    if (NULL == addr)
+        addr = xdl_dsym(handle, name, NULL);
+    return (uintptr_t)addr;
+}
+
+uintptr_t GetSymbolAddressEx(uintptr_t libAddr, const char* name)
+{
+    auto handle = GetLibHandle(GetLibFilePath(libAddr));
+    return NULL != handle ? GetSymbolAddress(handle, name) : NULL;
+}
+
+size_t GetSymbolSize(lib_h handle, const char* name)
+{
+    size_t size = NULL;
+    if (NULL == xdl_sym(handle, name, &size))
+        xdl_dsym(handle, name, &size);
+    return size;
+}
+
+size_t GetSymbolSizeEx(uintptr_t libAddr)
+{
+    xdl_info_t info;
+    void* cache = NULL;
+    if (xdl_addr((void*)libAddr, &info, &cache) == 0)
+        return NULL;
+    xdl_addr_clean(&cache);
+    return info.dli_ssize;
+}
+
+const char* GetSymbolName(uintptr_t libAddr)
+{
+    xdl_info_t info;
+    void* cache = NULL;
+    if (xdl_addr((void*)libAddr, &info, &cache) == 0)
+        return NULL;
+    xdl_addr_clean(&cache);
+    return info.dli_sname;
+}
+
+bool SetMemoryPermission(uintptr_t addr, size_t len, p_flag* type)
+{
+    if (addr == NULL || len == 0) return false;
+
+    int prot = PROT_READ | PROT_WRITE | PROT_EXEC;
+    if (type != NULL) {
+        prot = PROT_NONE;
+        if (type->bRead)
+            prot |= PROT_READ;
+        if (type->bWrite)
+            prot |= PROT_WRITE;
+        if (type->bExecute)
+            prot |= PROT_EXEC;
+        if (type->bPrivate)
+            prot |= PROT_NONE;
+        if (type->bShared)
+            prot = PROT_READ | PROT_WRITE;
+    }
+    unsigned long PageSize = sysconf(_SC_PAGESIZE);
+    const uintptr_t start = PAGE_START(addr, PageSize);
+    const uintptr_t end = PAGE_END((addr + len - 1), PageSize);
+    int ret = mprotect((void*)start, end - start, prot);
+    if (ret == -1)
+    {
+        HLOGE("Description Failed to set memory permission: %d-%s", errno, strerror(errno));
+        return false;
+    }
+    return true;
+}
+
+p_flag* GetMemoryPermission(uintptr_t addr, pid_t pid)
+{
+    char buffer[2048] = { 0 }, fname[2048] = { 0 };
+    uintptr_t start_address, end_address;
+    p_flag* type = (p_flag*)calloc(1, sizeof(p_flag));
+    if (pid < 0)
+        strcpy(fname, "/proc/self/maps");
+    else
+        snprintf(fname, sizeof(fname), "/proc/%d/maps", pid);
+
+    FILE* fp = fopen(fname, "rt");
+    if (fp != NULL) {
+        while (fgets(buffer, sizeof(buffer) - 1, fp)) {
+            if (strstr(buffer, "---p")) {
+                start_address = strtoul(strtok(buffer, "-"), NULL, 16);
+                end_address = strtoul(strtok(NULL, " "), NULL, 16);
+                if (addr >= start_address && addr <= end_address) {
+                    type->bPrivate = true;
+                    break;
+                }
+            }
+            else if (strstr(buffer, "r--p")) {
+                start_address = strtoul(strtok(buffer, "-"), NULL, 16);
+                end_address = strtoul(strtok(NULL, " "), NULL, 16);
+                if (addr >= start_address && addr <= end_address) {
+                    type->bRead = true;
+                    type->bPrivate = true;
+                    break;
+                }
+            }
+            else if (strstr(buffer, "rw-p")) {
+                start_address = strtoul(strtok(buffer, "-"), NULL, 16);
+                end_address = strtoul(strtok(NULL, " "), NULL, 16);
+                if (addr >= start_address && addr <= end_address) {
+                    type->bRead = true;
+                    type->bWrite = true;
+                    type->bPrivate = true;
+                    break;
+                }
+            }
+            else if (strstr(buffer, "r-xp")) {
+                start_address = strtoul(strtok(buffer, "-"), NULL, 16);
+                end_address = strtoul(strtok(NULL, " "), NULL, 16);
+                if (addr >= start_address && addr <= end_address) {
+                    type->bRead = true;
+                    type->bExecute = true;
+                    type->bPrivate = true;
+                    break;
+                }
+            }
+            else if (strstr(buffer, "rwxp")) {
+                start_address = strtoul(strtok(buffer, "-"), NULL, 16);
+                end_address = strtoul(strtok(NULL, " "), NULL, 16);
+                if (addr >= start_address && addr <= end_address) {
+                    type->bRead = true;
+                    type->bWrite = true;
+                    type->bExecute = true;
+                    type->bPrivate = true;
+                    break;
+                }
+            }
+            else if (strstr(buffer, "rw-s")) {
+                start_address = strtoul(strtok(buffer, "-"), NULL, 16);
+                end_address = strtoul(strtok(NULL, " "), NULL, 16);
+                if (addr >= start_address && addr <= end_address) {
+                    type->bRead = true;
+                    type->bWrite = true;
+                    type->bShared = true;
+                    break;
+                }
+            }
+        }
+        fclose(fp);
+    }
+    return type;
+}
+
+void WriteMemory(void* addr, void* data, size_t size, bool vp)
+{
+    Unprotect((uintptr_t)addr, vp ? size : 0);
+    memcpy(addr, data, size);
+    cacheflush((uintptr_t)addr, (uintptr_t)addr + size, 0);
+}
+
+void* ReadMemory(void* addr, void* data, size_t size, bool vp)
+{
+    Unprotect((uintptr_t)addr, vp ? size : 0);
+    memcpy(data, addr, size);
+    return data;
+}
+
+void MemoryFill(void* addr, uint8_t value, size_t size, bool vp)
+{
+    Unprotect((uintptr_t)addr, vp ? size : 0);
+    memset(addr, value, size);
+    cacheflush((uintptr_t)addr, (uintptr_t)addr + size, 0);
+}
+
+void PLTInternal(void* addr, void* func, void** original)
+{
+    if (addr == NULL || func == NULL) return;
+    Unprotect((uintptr_t)addr, sizeof(uintptr_t));
+    if (original != NULL)
+        *((uintptr_t*)original) = *(uintptr_t*)addr;
+    *(uintptr_t*)addr = (uintptr_t)func;
+    cacheflush((uintptr_t)addr, (uintptr_t)addr + sizeof(uintptr_t), 0);
+}
---
+++ AndroidArmHook/AndroidArmHook.h
@@ -0,0 +1,263 @@
+#ifndef ANDROID_ARMHOOK_H
+#define ANDROID_ARMHOOK_H
+
+#ifndef __ANDROID__
+#error error
+#else
+#if !(defined __arm__) && !(defined __aarch64__)
+#error error
+#endif
+#endif // __ANDROID__
+
+#ifdef __cplusplus
+extern "C" {
+#endif                                           
+
+#include <unistd.h> //lseek PAGE_SIZE
+
+#define PAGE_START(addr, size) ((uintptr_t)addr & ~(size - 1))                             
+#define PAGE_END(addr, size) PAGE_START(addr + size - 1, size)
+#ifdef __arm__
+#define SET_BIT0(addr) (addr | 1)
+#define CLEAR_BIT0(addr) (addr & 0xFFFFFFFE)
+#define TEST_BIT0(addr) (addr & 1)
+#define GET_INST_SET(addr) (TEST_BIT0(addr) ? i_set::$THUMB32 : i_set::$ARM)
+#elif __aarch64__
+#define cacheflush(addr, byte, n) __builtin___clear_cache((char*)addr, (char*)byte)
+#endif
+	
+	typedef void* lib_h;
+	typedef struct PermissionFlags
+	{
+		int8_t bRead : 1;
+		int8_t bWrite : 1;
+		int8_t bExecute : 1;
+		int8_t bShared : 1;
+		int8_t bPrivate : 1;
+		int8_t align : 3;
+	} p_flag;
+
+	typedef enum { NONE, $THUMB32, $ARM, $ARM64 } i_set; //InstructionSet
+
+	/*
+	* Get the starting address where the library was first loaded into memory(image base).
+	* libName: Library filename.
+	* pid: Process ID(Defaults to -1, the current process).
+	* return: Absolute address.
+	*/
+	uintptr_t GetLibBase(const char* libName, pid_t pid = -1);
+
+	/*
+	* Get the length of the library loaded into memory.
+	* libName: Library filename.
+	* pid: Process ID(Defaults to -1, the current process).
+	* return: Library length.
+	*/
+	size_t GetLibLength(const char* libName, pid_t pid = -1);
+
+	/*
+	* Get a handle to the library.
+	* libName: Library filename or pathname.
+	* return: Library handle.
+	*/
+	lib_h GetLibHandle(const char* libName);
+
+	/*
+	* Close a library loaded by the 'GetLibHandle' function.
+	* handle: Library handle (from 'GetLibHandle' func).
+	*/
+	void CloseLib(lib_h handle);
+
+	/*
+	* According to the handle of the library, Get the starting address where the library was first loaded into memory(image base).
+	* handle: Library handle (from 'GetLibHandle' func).
+	* return: Absolute address.
+	*/
+	uintptr_t GetLibBaseFromHandle(lib_h handle);
+
+	/*
+	* Get the file path of the library based on the memory address of the library.
+	* libAddr: Specifies a library-wide memory address.
+	* return: Library file absolute path.
+	*/
+	const char* GetLibFilePath(uintptr_t libAddr);
+
+	/*
+	* Get the file path of the library based on the handle of the library.
+	* handle: Library handle (from 'GetLibHandle' func).
+	* return: Library file absolute path.
+	*/
+	const char* GetLibFilePathFromHandle(lib_h handle);
+
+	/*
+	* Get the file size of the library based on the handle of the library.
+	* handle: Library handle (from 'GetLibHandle' func).
+	* return: Library file size.
+	*/
+	size_t GetLibFileSize(lib_h handle);
+
+	/*
+	* Get the absolute address of symbol based on the symbol.
+	* handle: Library handle(from 'GetLibHandle' func).
+	* name: Symbol name.
+	* return: Symbol absolute address.
+	*/
+	uintptr_t GetSymbolAddress(lib_h handle, const char* name);
+
+	/*
+	* Get the absolute address of symbol based on the symbol.
+	* libAddr: Specifies a library-wide memory address.
+	* name: Symbol name.
+	* return: Symbol absolute address.
+	*/
+	uintptr_t GetSymbolAddressEx(uintptr_t libAddr, const char* name);
+
+	/*
+	* Get the size of the symbol code in memory based on the symbol(size in ELF, not the symbol itself).
+	* handle: Library handle(from 'GetLibHandle' func).
+	* name: Symbol name.
+	* return: Symbol size.
+	*/
+	size_t GetSymbolSize(lib_h handle, const char* name);
+
+	/*
+	* According to the memory address of the library, obtain the size of the symbol code where the address is located in memory (the size in ELF, not the symbol itself).
+	* libAddr: Specifies a library-wide memory address.
+	* name: Symbol name.
+	* return: Symbol size.
+	*/
+	size_t GetSymbolSizeEx(uintptr_t libAddr);
+
+	/*
+	* Get the symbolic name where the address is located.
+	* libAddr: Specifies a library-wide memory address.
+	* return: Symbol name.
+	*/
+	const char* GetSymbolName(uintptr_t libAddr);
+
+	/*
+	* Set the access permission of the specified length of memory.
+	* addr: The memory address for which permissions need to be set.
+	* len: Address length.
+	* type: Permission type(see 'PermissionFlags'), If NULL, the default settings are readable, writable, executable.
+	* return: Returns 'true' on success, 'false' on failure.
+	*/
+	bool SetMemoryPermission(uintptr_t addr, size_t len, p_flag* type);
+
+	/*
+	* Cancel the protection permission of the specified length of memory, set to readable, writable, executable.
+	* addr: The memory address for which permissions need to be set.
+	* len: Address length(Defaults to a full page of memory).
+	* return: Returns 'true' on success, 'false' on failure.
+	*/
+	inline bool Unprotect(uintptr_t addr, size_t len = PAGE_SIZE)
+	{
+		return SetMemoryPermission(addr, len, NULL);
+	}
+
+	/*
+	* Get access rights to process memory address.
+	* addr: The memory address where permission needs to be obtained.
+	* pid: Process ID(Defaults to -1, the current process).
+	* return: Permission type(see 'PermissionFlags').
+	*/
+	p_flag* GetMemoryPermission(uintptr_t addr, pid_t pid = -1);
+
+	/*
+	* Check whether the memory address access permission of the current process is executable.
+	* addr: The memory address that needs to be checked.
+	* return: Judging success returns 'true', 'false' on failure.
+	*/
+	inline bool IsAddrExecute(uintptr_t addr)
+	{
+		return GetMemoryPermission(addr)->bExecute ? true : false;
+	}
+
+	/*
+	* Write the content of the specified 'size' 'data' into memory, and set whether to cancel the memory protection permission.
+	* addr: The memory address where data needs to be written.
+	* data: The content of the data to be written.
+	* size: Length of data written.
+	* vp: 'true' to cancel the memory protection permission, 'false' to keep the original permission. Defaults to 'true' (because ARM's memory is more strict and safe).
+	*/
+	void WriteMemory(void* addr, void* data, size_t size, bool vp = true);
+
+	/*
+	* Read the memory of the specified 'size' at the address, save the content to 'data', and set whether to cancel the memory protection permission.
+	* addr: The memory address to read data from.
+	* data: Where to save the data.
+	* size: Length of read data.
+	* vp: 'true' to cancel the memory protection permission, 'false' to keep the original permission. Defaults to 'true' (because ARM's memory is more strict and safe).
+	* return: Read data.
+	*/
+	void* ReadMemory(void* addr, void* data, size_t size, bool vp = true);
+
+	/*
+	* Set all the contents of the specified size of memory to 'value', and set whether to cancel the memory protection permission.
+	* addr: The memory address that needs to be set.
+	* value: The value to be written.
+	* size: Number of bytes to write.
+	* vp: 'true' to cancel the memory protection permission, 'false' to keep the original permission. Defaults to 'true' (because ARM's memory is more strict and safe).
+	*/
+	void MemoryFill(void* addr, uint8_t value, size_t size, bool vp = true);
+
+	/*
+	* PLT Hook. But it is internal, it is not a real PLT Hook, it is actually a GOT Hook,
+	  the hook is implemented by replacing the function pointer of the .GOT segment, and the original function pointer is retained.
+	* addr: Requires the location of the Hook, the absolute address of the .GOT segment.
+	* func: New function address.
+	* original: Original function pointer, can be NULL.
+	*/
+	void PLTInternal(void* addr, void* func, void** original);
+
+#ifdef __cplusplus
+	}
+
+	/*
+	* Write any type of value to memory.
+	*/
+	template <typename T1, typename T2>
+	inline static void WriteMemory(T2 addr, T1 value, bool vp = true)
+	{
+		WriteMemory((void*)addr, &value, sizeof(T1), vp);
+	}
+
+	/*
+	* Read any type of value from memory.
+	*/
+	template <typename T1, typename T2>
+	inline static T1 ReadMemory(T2 addr, bool vp = true)
+	{
+		if (vp) Unprotect((uintptr_t)addr, sizeof(T1));
+		return *reinterpret_cast<T1*>(addr);
+	}
+
+	/*
+	* GOTHook template, complete type conversion.
+	*/
+	template <class A, class B, class C>
+	inline static void GOTHook(A addr, B func, C original)
+	{
+		return PLTInternal(reinterpret_cast<void*>(addr), reinterpret_cast<void*>(func), reinterpret_cast<void**>(original));
+	}
+	template <class A, class B>
+	inline static void GOTHook(A addr, B func)
+	{
+		return PLTInternal(reinterpret_cast<void*>(addr), reinterpret_cast<void*>(func), NULL);
+	}
+
+	/*
+	* InlineHook template, complete type conversion.
+	*/
+	template <class A, class B, class C>
+	inline static void InlineHook(A addr, B func, C original)
+	{
+		return InlineHook(reinterpret_cast<void*>(addr), reinterpret_cast<void*>(func), reinterpret_cast<void**>(original));
+	}
+	template <class A, class B>
+	inline static void InlineHook(A addr, B func)
+	{
+		return InlineHook(reinterpret_cast<void*>(addr), reinterpret_cast<void*>(func), NULL);
+	}
+#endif
+#endif
