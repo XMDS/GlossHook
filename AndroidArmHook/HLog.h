50cadcd1c6ac2f29e086a6504f433af2ccbc823b
晓梦大师/XMDS <2841824304@qq.com>
2023-01-29 13:44:37 +0800

    add HLog.h

---
+++ AndroidArmHook/HLog.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <android/log.h>
+
+#define HTAG "ARMHook"
+#define WLOGI(text) ((void)__android_log_write(ANDROID_LOG_INFO, HTAG, text))
+#define WLOGE(text) ((void)__android_log_write(ANDROID_LOG_ERROR, HTAG, text))
+#define WLOGD(text) ((void)__android_log_write(ANDROID_LOG_DEBUG, HTAG, text))
+#define WLOGW(text) ((void)__android_log_write(ANDROID_LOG_WARN, HTAG, text))
+#define HLOGI(text, ...) ((void)__android_log_print(ANDROID_LOG_INFO, HTAG, text, __VA_ARGS__))
+#define HLOGE(text, ...) ((void)__android_log_print(ANDROID_LOG_ERROR, HTAG, text, __VA_ARGS__))
+#define HLOGD(text, ...) ((void)__android_log_print(ANDROID_LOG_DEBUG, HTAG, text, __VA_ARGS__))
+#define HLOGW(text, ...) ((void)__android_log_print(ANDROID_LOG_WARN, HTAG, text, __VA_ARGS__))
