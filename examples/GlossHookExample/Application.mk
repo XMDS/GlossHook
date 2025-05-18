3be92b41fd0b1b5e918940c50c92209780f1f557
XMDS <2841824304@qq.com>
2025-05-18 21:24:35 +0800

    rebuild 1.9.5

--- examples/GlossHookExample/Application.mk
+++ examples/GlossHookExample/Application.mk
@@ -1,13 +1,9 @@
 NDK_TOOLCHAIN_VERSION := clang
-
 APP_STL := c++_static
-
 APP_ABI := armeabi-v7a arm64-v8a
-
 ifeq ($(NDK_DEBUG), 1)
 	APP_OPTIM := debug
 else
 	APP_OPTIM := release
 endif
-
 APP_PLATFORM := android-21
